6aa18f7f0660b742fb3009cd5742bbc865c8ec9e
David Lavelle <dlavelle7@yahoo.ie>
2016-10-03 21:20:29 +0100

    finished sieve of eratosthenes

--- python/sieve_of_eratosthenes.py
+++ python/sieve_of_eratosthenes.py
@@ -3,40 +3,25 @@
 
 Find all the prime numbers less than or equal to a given integer n
 """
-import sys
 
 
-def sieve(num):
+def sieve(n):
     crossed = set()
-    # Start at the smallest prime 2
-    for p in xrange(2, num):
-        # Iterate on non crossed out numbers
+    # Start from the smallest prime numer 2 to n
+    for p in xrange(2, n):
+        # Iterate over non crossed out numbers
         if p in crossed:
             continue
-        # Cross out all numbers from 2p in steps of p
-        found = False
-        for i in xrange(p * 2, num, p):
-            crossed.add(i)
-            found = True
-        # Keep crossing out until we get a clean pass
-        if not found:
+        # Break if every pth number is crossed out already
+        if p * p > n:
             break
+        # Cross out all numbers from 2p in steps of p (i.e. multiples of p)
+        for i in xrange(p * 2, n, p):
+            crossed.add(i)
 
-    return set(xrange(2, num)) - crossed
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print "You must pass exactly one argument to this script."
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except ValueError:
-        print "You must pass an integer argument to this script."
-        sys.exit(1)
-    else:
-        sieve(n)
+    # Return non crossed out numbers (prime numbers)
+    return set(xrange(2, n)) - crossed
 
 
-# Tests
-assert set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]) == sieve(30)
+# Test
+assert set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43]) == sieve(45)
