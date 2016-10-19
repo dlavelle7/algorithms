67cdf374be02dd6d299265743c1393652eb2c250
David Lavelle <d.lavelle7@yahoo.ie>
2016-10-19 20:20:03 +0100

    some improvements

--- python/online_tests/spoj_prime1.py
+++ python/online_tests/spoj_prime1.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
+"""Prime number generator.
+
+Input:
+The input begins with the number t of test cases in a single line (t<=10). In
+each of the next t lines there are two numbers m and n (1 <= m <= n <=
+1000000000, n-m<=100000) separated by a space.
+
+Output
+For every test case print all prime numbers p such that m <= p <= n, one number
+per line, test cases separated by an empty line.
+"""
 import sys
+import time
 
 
-# FIXME: Runtime error
-def sieve_range(start, end):
-    uncrossed = [True] * (end + 1)
+def sieve(end):
+    # FIXME: Given the max size of m <= 1bln, this is too much memory
+    uncrossed = [True for i in xrange(end + 1)]
     uncrossed[0] = uncrossed[1] = False
     for p in xrange(2, end + 1):
         if p * p > end:
@@ -12,17 +24,29 @@ def sieve_range(start, end):
         if uncrossed[p]:
             for i in xrange(p**2, end + 1, p):
                 uncrossed[i] = False
-
-    for num in xrange(start, end + 1):
-        if uncrossed[num]:
-            print num
+    return uncrossed
 
 
 if __name__ == "__main__":
+    start = time.time()  # TODO: Remove XXX
     no_of_tcs = int(sys.stdin.readline())
+    # Find the biggest n
+    ns = set()
+    test_cases = []
     for tc in xrange(no_of_tcs):
-        # TODO: Could find max of all tc ranges, find all the primes from
-        # 2 - max, then all the other tcs are subsets of that. No need to redo???
         m, n = sys.stdin.readline().split()
-        sieve_range(int(m), int(n))
+        ns.add(int(n))
+        test_cases.append((int(m), int(n)))
+    big_n = max(ns)
+
+    # Create a sieve from 2 to big_n
+    master_sieve = sieve(big_n)
+
+    # For each test case range, find primes using master sieve
+    for m, n in test_cases:
+        for num in xrange(m, n + 1):
+            if master_sieve[num]:
+                print num
         print ""
+    end = time.time()  # TODO: Remove XXX
+    print "Execution time %s" % (end - start)  # TODO: Remove XXX
--- python/primes.py
+++ python/primes.py
@@ -2,13 +2,15 @@
 """Prime numbers.
 
 Find all the prime numbers less than or equal to a given integer n.
+
+A prime number is a number greater than 1 that has no positive divisors other
+than 1 and itself.
+
 See sieve_of_eratosthenes.py for more prime number algorithms.
 """
-import time
-
-# TODO: Tidy up
 
 def simple_primes(n):
+    """Basic prime number algorithm."""
     primes = set()
     for i in xrange(2, n + 1):
         for j in xrange(2, i):
@@ -19,8 +21,7 @@ def simple_primes(n):
     return primes
 
 
-def better_simple_primes(n):
-    # TODO: Caching?
+def simple_primes_2(n):
     primes = set()
     for i in xrange(2, n + 1):
         j = 2
@@ -40,6 +41,6 @@ assert set([2, 3, 5, 7, 11, 13, 17, 19, 23]) == simple_primes(23)
 assert set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]) == simple_primes(30)
 
 # Test better_simple_primes
-assert set([2]) == better_simple_primes(2)
-assert set([2, 3, 5, 7, 11, 13, 17, 19, 23]) == better_simple_primes(23)
-assert set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]) == better_simple_primes(30)
+assert set([2]) == simple_primes_2(2)
+assert set([2, 3, 5, 7, 11, 13, 17, 19, 23]) == simple_primes_2(23)
+assert set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]) == simple_primes_2(30)
