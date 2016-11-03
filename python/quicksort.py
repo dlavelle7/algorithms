9a134cf002b963294fdd4cf79ccf7c43bf51f670
David Lavelle <d.lavelle7@yahoo.ie>
2016-11-03 20:40:19 +0000

    quicksort algorithm

---
+++ python/quicksort.py
@@ -0,0 +1,81 @@
+"""Quicksort is an efficient 'divide and conquer' sorting algorithm.
+
+On average, quicksort has takes O(n log n) comparisons to sort n items. It's
+not a stable sort, meaning that the order of equal elements is not preserved.
+Quick sort is memory efficient as it sorts the array 'in place'
+(unlike mergesort).
+
+Algorithm steps:
+    1. Select a 'pivot' value.
+    2. Move the other elements to the appropriate side of the pivot, which
+       places the pivot in its correct position.
+    3. Repeat steps 1 & 2 with both sides of the list from the pivot until the
+       sublists are single element sorted lists.
+"""
+
+
+def quicksort(a_list):
+    # Use the first element in the list as the pivot point
+    recursive_quicksort(a_list, 0, len(a_list) - 1)
+
+
+def recursive_quicksort(a_list, pivot_point, right):
+    # If the length of the list is 1, then it's already sorted
+    if right > pivot_point:
+        # Find 'split point' - the pivots correct position.
+        split_point = partition(a_list, pivot_point, right)
+        # Recursively sort both sides of the list from split point
+        recursive_quicksort(a_list, pivot_point, split_point - 1)
+        recursive_quicksort(a_list, split_point + 1, right)
+
+
+def partition(a_list, pivot_point, right):
+    left = pivot_point + 1
+
+    # Find elements in list that need to be moved in realtion to the pivot
+    while True:
+        # Starting from the left stop at a value that is greater than the pivot
+        while left <= right and a_list[left] < a_list[pivot_point]:
+            left += 1
+
+        # Then from the right stop at a value that is less than the pivot
+        while right >= left and a_list[right] > a_list[pivot_point]:
+            right -= 1
+
+        # If there are no elements to be moved, 'right' is the pivots position
+        if right < left:
+            break
+
+        # Move elements to appropriate side of pivot
+        a_list[left], a_list[right] = a_list[right], a_list[left]
+
+        # We've just swapped them so no need to compare them again
+        left += 1
+        right -= 1
+
+    # Put the pivot in its correct place in the list
+    a_list[pivot_point], a_list[right] = a_list[right], a_list[pivot_point]
+
+    return right
+
+
+# Testing
+a_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+quicksort(a_list)
+assert a_list == [17, 20, 26, 31, 44, 54, 55, 77, 93]
+
+a_list = []  # empty list is sorted
+quicksort(a_list)
+assert a_list == []
+
+a_list = [1]  # single element list is sorted
+quicksort(a_list)
+assert a_list == [1]
+
+a_list = [1, 2, 3]  # already sorted list
+quicksort(a_list)
+assert a_list == [1, 2, 3]
+
+a_list = [9, 9, 3, 2, 4, 8, 7, 6, 10, 1, 2, 6, 6, 7, 5, 4, 0, 8]
+quicksort(a_list)
+assert a_list == [0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10]
