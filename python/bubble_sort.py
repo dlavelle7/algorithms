a3a650c39d55ad22d4c612bfc24a351c98042abf
David Lavelle <davidlavelle1@hotmail.com>
2019-02-18 12:37:57 +0000

    flake8 lint

--- python/bubble_sort.py
+++ python/bubble_sort.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python
 
+
 def bubble_sort(sequence):
     unsorted = True
-    while unsorted: # loop until we make one pass with no swaps
+    # loop until we make one pass with no swaps
+    while unsorted:
         unsorted = False
-        for i in range(len(sequence) -1):
+        # iterate from index 0 -> 2nd last item in list as comparing with i + 1
+        for i in range(len(sequence) - 1):
             if sequence[i] > sequence[i + 1]:
-                unsorted = True # swap took place, need to iterate again
+                unsorted = True  # swap took place, need to iterate again
                 # Swap in one line, no need for temp variable
                 sequence[i + 1], sequence[i] = sequence[i], sequence[i + 1]
     return sequence
 
+
 if __name__ == "__main__":
-    # Test cases
-    test_list = [2, 4, 5, 3, 6, 1]
+    # Test cases (Note: sorted() returns a new list)
+    test_list = []
     assert bubble_sort(test_list) == sorted(test_list)
     test_list = [2]
     assert bubble_sort(test_list) == sorted(test_list)
     test_list = [3, 4]
     assert bubble_sort(test_list) == sorted(test_list)
-    test_list = []
+    test_list = [2, 4, 5, 3, 6, 1]
     assert bubble_sort(test_list) == sorted(test_list)
     test_list = [2, 4, 5, 3, 2, 1]
     assert bubble_sort(test_list) == sorted(test_list)
