6ac681e0784db6ae3c12f32923a048b82f87c09e
David Lavelle <davidlavelle1@hotmail.com>
2019-02-28 21:10:52 +0000

    added docstring and floor division

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python
+"""Binary search algorithm.
+
+Finds the position of a given target in a sorted array.
+Binary search has a worst case and average performance of O(log n).
+
+Each operation compares the middle value of the list with the target, the half
+where the target cannot be is discarded and this operation is repeated until
+the middle value equals the target or not.
+"""
 
 
 def binary_search(number, sorted_list):
     left = 0
     right = len(sorted_list) - 1
-    mid = (right + left) / 2
+    mid = (right + left) // 2  # floor division
     while left <= right and sorted_list[mid] != number:
         if number < sorted_list[mid]:
             right = mid - 1
         else:
             left = mid + 1
-        mid = (right + left) / 2
+        mid = (right + left) // 2  # floor division
     return mid if sorted_list[mid] == number else None
 
 
