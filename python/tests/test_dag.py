6b2fd47241db9797cccdcae739afa90e0eb8f3da
David Lavelle <davidlavelle1@hotmail.com>
2019-03-03 18:05:51 +0000

    added negative test

--- python/tests/test_dag.py
+++ python/tests/test_dag.py
@@ -12,3 +12,6 @@ class TestDirectedAcyclicGraph(TestCase):
     def test_find_path_02(self):
         self.assertListEqual(['E', 'F', 'C', 'D'],
                              find_path(acyclic_graph, 'E', 'D'))
+
+    def test_find_path_03(self):
+        self.assertIsNone(find_path(acyclic_graph, 'A', 'F'))
