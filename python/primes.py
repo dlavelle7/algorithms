099e63e3c69af7e1638b16c863312bfde5052f6b
David Lavelle <davidlavelle1@hotmail.com>
2019-03-03 12:21:57 +0000

    primes comments

--- python/primes.py
+++ python/primes.py
@@ -3,8 +3,9 @@
 
 Find all the prime numbers less than or equal to a given integer n.
 
-A prime number is a number greater than 1 that has no positive divisors other
-than 1 and itself.
+A prime number is a whole number greater than 1, whose only factors are 1 and
+itself. A factor is a whole number that can be evenly divided into another
+number.
 
 See sieve_of_eratosthenes.py for more prime number algorithms.
 """
@@ -14,6 +15,7 @@ def simple_primes(n):
     """Basic prime number algorithm."""
     primes = set()
     for i in xrange(2, n + 1):
+        # check for factors of i from all the numbers between 2 and i
         for j in xrange(2, i):
             if i % j == 0:
                 break
@@ -23,10 +25,15 @@ def simple_primes(n):
 
 
 def simple_primes_2(n):
+    """Optimization, only check for primes up to half of i.
+
+    e.g. Find the factors of 10:
+    - only check from 2 -> 5, no more divisors of 10 after 5 with modulus 0.
+    """
     primes = set()
     for i in xrange(2, n + 1):
         j = 2
-        top = i / 2  # if j > i you will never get modulus 0 (e.g. 30 / 16)
+        top = i // 2  # if j > i/2 you will never get modulus 0 (e.g. 10 / 6)
         while j <= top:
             if i % j == 0:
                 break
