480c6c651a77e9678d42f129b80c0f018752feff
David Lavelle <davidlavelle1@hotmail.com>
2019-03-04 19:06:46 +0000

    test is dag

--- python/directed_acyclic_graph.py
+++ python/directed_acyclic_graph.py
@@ -8,18 +8,8 @@ edges (or arcs) have a direction.
 In acyclical graphs, there is no way to start at any node, and loop back around
 to that node again. Acyclic graphs have a topological ordering.
 """
+from collections import defaultdict
 
-
-# Nodes are the keys, and directed edges are the values
-acyclic_graph = {
-    'A': ['B', 'C'],
-    'B': ['G', 'D', 'C'],
-    'C': ['D'],
-    'D': ['C'],
-    'E': ['F'],
-    'F': ['C'],
-    'G': [],
-}
 
 
 def find_path(graph, start_node, end_node, path=[]):
@@ -45,4 +35,31 @@ def find_path(graph, start_node, end_node, path=[]):
 
 # TODO: Find the shortest path from one node to another
 
-# TODO: Test if this graph is an acyclic graph or not
+def has_cycles(graph):
+    """A directed graph is acyclic if it can be topologically sorted."""
+    # count the incoming edges of each node
+    incoming = defaultdict(int)
+    for children in graph.values():
+        for child in children:
+            incoming[child] += 1
+
+    # start with a node that has no incoming edges
+    edgeless_nodes = [node for node in graph if node not in incoming]
+
+    topsorted = []
+    while edgeless_nodes:
+        node = edgeless_nodes.pop()
+        topsorted.append(node)
+
+        # remove the edges for this node
+        for dependency in graph[node]:
+            incoming[dependency] -= 1
+            # check if this node could go next
+            if incoming[dependency] == 0:
+                edgeless_nodes.append(dependency)
+
+    # if there are any incoming edges left, the graph has cycles
+    edges_left = any(incoming_count > 0 for incoming_count in incoming.values())
+    if not edges_left:
+        print(f"DAG Topsort order: {topsorted}")
+    return edges_left
