ed6aaf2805e19df2f029d9625d84f95301b02a3e
David Lavelle <davidlavelle1@hotmail.com>
2019-03-10 11:39:41 +0000

    find working

--- python/binary_search_tree.py
+++ python/binary_search_tree.py
@@ -25,12 +25,6 @@ class Node:
     def __repr__(self):
         return self.name
 
-    def __gt__(self, node):
-        return self.value > node.value
-
-    def __lt__(self, node):
-        return self.value < node.value
-
     @property
     def name(self):
         return f"Node {self.value}"
@@ -48,18 +42,30 @@ class BinarySearchTree:
             self._add(node, self.root)
 
     def _add(self, new_node, existing_node):
-        if new_node > existing_node:
+        if new_node.value > existing_node.value:
             if existing_node.right is None:
                 existing_node.right = new_node
             else:
                 self._add(new_node, existing_node.right)
-        elif new_node < existing_node:
+        elif new_node.value < existing_node.value:
             if existing_node.left is None:
                 existing_node.left = new_node
             else:
                 self._add(new_node, existing_node.left)
 
-# TODO: Search for value method
+    def find(self, value):
+        return self._find(value, self.root)
+
+    def _find(self, value, search_node):
+        # TODO: Find path?
+        # TODO: Find out how my levels down it was
+        if value == search_node.value:
+            return search_node
+        elif value > search_node.value and search_node.right is not None:
+            return self._find(value, search_node.right)
+        elif value < search_node.value and search_node.left is not None:
+            return self._find(value, search_node.left)
+
 
 def create_binary_search_tree():
     bs_tree = BinarySearchTree()
@@ -68,7 +74,7 @@ def create_binary_search_tree():
     return bs_tree
 
 
-def render_binary_search_tree():
+def render_binary_search_tree(bs_tree):
     dot = Digraph("Binary Search Tree")
 
     def draw_node(node):
@@ -80,11 +86,7 @@ def render_binary_search_tree():
             dot.edge(node.name, node.right.name)
             draw_node(node.right)
 
-    bs_tree = create_binary_search_tree()
     draw_node(bs_tree.root)
 
     print(dot.source)
     dot.render()
-
-
-render_binary_search_tree()
--- python/tests/test_binary_search_tree.py
+++ python/tests/test_binary_search_tree.py
@@ -6,6 +6,7 @@ from python.binary_search_tree import BinarySearchTree, Node
 class TestBinarySearchTree(TestCase):
 
     def test_binary_search_tree(self):
+        # test adding
         bs_tree = BinarySearchTree()
         root = Node(10)
         bs_tree.add(root)
@@ -22,3 +23,8 @@ class TestBinarySearchTree(TestCase):
         node_22 = Node(22)
         bs_tree.add(node_22)
         self.assertIs(node_14.right, node_22)
+
+        # test finding
+        self.assertIs(node_14, bs_tree.find(14))
+
+        self.assertIsNone(bs_tree.find(100))
