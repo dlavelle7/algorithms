cab35c143aef579ca770d589b0c57de360066dca
David Lavelle <davidlavelle1@hotmail.com>
2019-03-24 18:11:18 +0000

    fixed bug in test

--- python/tests/test_sorts.py
+++ python/tests/test_sorts.py
@@ -7,22 +7,16 @@ from python.quick_sort import quick_sort
 
 class TestSorts(TestCase):
 
-    # Unsorted lists to test against
-    test_cases = [
-        [],
-        [2],
-        [3, 4],
-        [2, 4, 5, 3, 6, 1, 100, 80],
-        [2, -4, -5, 3, -3, 1, -200, 101],
-        [0, 0, -1, -0.507, 3, 2, 3, 5, 99, 98, 97, 99, 0, -0.5066],
-    ]
-
-    # Algorithms to test (add new ones here)
-    test_algorithms = (
-        bubble_sort,
-        insertion_sort,
-        quick_sort,
-    )
+    def setUp(self):
+        # Unsorted lists to test against
+        self.test_cases = [
+            [],
+            [2],
+            [3, 4],
+            [2, 4, 5, 3, 6, 1, 100, 80],
+            [2, -4, -5, 3, -3, 1, -200, 101],
+            [0, 0, -1, -0.507, 3, 2, 3, 5, 99, 98, 97, 99, 0, -0.5066],
+        ]
 
     def _assert_sorts_in_place(self, algorithm, test_list):
         """Assert the list has been sorted in place by the algorithm."""
@@ -30,7 +24,14 @@ class TestSorts(TestCase):
         algorithm(test_list)
         self.assertListEqual(sorted_list, test_list)
 
-    def test_sorting(self):
-        for algorithm in self.test_algorithms:
-            for test_list in self.test_cases:
-                self._assert_sorts_in_place(algorithm, test_list)
+    def test_bubble_sort(self):
+        for test_list in self.test_cases:
+            self._assert_sorts_in_place(bubble_sort, test_list)
+
+    def test_insertion_sort(self):
+        for test_list in self.test_cases:
+            self._assert_sorts_in_place(insertion_sort, test_list)
+
+    def test_quick_sort(self):
+        for test_list in self.test_cases:
+            self._assert_sorts_in_place(quick_sort, test_list)
